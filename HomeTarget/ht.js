4163bad37c99ecf6cd7dbe5c22d054512657484a
DankeNguyen369 <63439170+DankeNguyen369@users.noreply.github.com>
2021-04-03 17:51:15 +0700

    Add files via upload

---
+++ HomeTarget/ht.js
@@ -0,0 +1,96 @@
+// var a;
+
+// // "Undefined value"
+// if (typeof a == "undefined")
+//     console.log("Undefined value");
+// else
+//     console.log("Defined value");
+
+// var b = null;
+
+// // "Null value"
+// if (b === null)
+//     console.log("Null value");
+// else
+//     console.log("Not null value");
+
+// var a = 5;
+
+// // "Numeric value"
+// if (typeof a == "number")
+//     console.log("Numeric value");
+// else
+//     console.log("Not a number");
+
+// var a = 'Tree';
+
+// // "String value"
+// if (typeof a == "string")
+//     console.log("String value");
+// else
+//     console.log("Not a string");
+
+// var a = true;
+
+// // "Boolean value"
+// if (typeof a == "boolean")
+//     console.log("Boolean value");
+// else
+//     console.log("Not a boolean");
+
+// var a = { key: "value" };
+
+// // "Object"
+// if (typeof a == "object")
+//     console.log("Object");
+// else
+//     console.log("Not an object");
+
+
+// var a = [1, 2, 3];
+
+// // "Represents an array"
+// if (a instanceof Array)
+//     console.log("Represents an array");
+// else
+//     console.log("Does not represent an array");
+
+
+// const greeting = {
+//     message: "Hello",
+//     number: "five"
+// }
+
+// greeting.message = "say Hello instead";
+// console.log(greeting); // {message:"say Hello instead",number:"five"}
+
+// alert('2 vạch rồi anh ơi');
+
+// var n=prompt("Hi there, name please!", '')
+// alert("Ypur Parent must proud of you,", n )
+
+// const num1 = (prompt("Họ"))
+// console.log(num1)
+// const num2 = (prompt("Tên"))
+// console.log(num2)
+// const num3 = (prompt("Tên Đệm/Lót"))
+// console.log(num3)
+// let fullName = `my full name  : ${num1} ${num3} ${num2}`
+// confirm("Tên bạn là: " +  fullName);
+
+
+
+
+// const a = parseInt(prompt("side length of the square:"))
+// console.log("the square area is", a*a);
+
+// const r = parseInt(prompt("radius of the circle:"))
+// console.log("the circle area is", a*a*Math.PI);
+
+// const c = parseInt(prompt("temp you feel in Celcius:"))
+// console.log("temp you feel in Celcius:", (c*9/5)+32);
+
+let message = "code the change!";
+let studentCount = 0;
+console.log(message);
+console.log(studentCount);
